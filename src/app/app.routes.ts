e1957d91daea9aedc5fd4f35e9e2ce6e001f067c
Alejandro Portilla <portillaisrael9@gmail.com>
2025-10-13 00:18:19 -0500

    "chore: Cambios en Home"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,12 @@ import { ServicesComponent } from '@app/pages/services/services.component';
 import { AboutComponent } from '@app/pages/about/about.component';
 import { ContactComponent } from '@app/pages/contact/contact.component';
 
+// Importar los nuevos componentes de detalle
+import { DesarrolloArquitecturaComponent } from '@app/pages/services/desarrollo-arquitectura/desarrollo-arquitectura.component';
+import { AutomatizacionConectividadComponent } from '@app/pages/services/automatizacion-conectividad/automatizacion-conectividad.component';
+import { ConsultoriaMigracionComponent } from '@app/pages/services/consultoria-migracion/consultoria-migracion.component';
+import { ExtensionEquipoComponent } from '@app/pages/services/extension-equipo/extension-equipo.component';
+
 export const routes: Routes = [
   {
     path: '',
@@ -12,10 +18,13 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'services', component: ServicesComponent },
+      { path: 'services/desarrollo-arquitectura', component: DesarrolloArquitecturaComponent },
+      { path: 'services/automatizacion-conectividad', component: AutomatizacionConectividadComponent },
+      { path: 'services/consultoria-migracion', component: ConsultoriaMigracionComponent },
+      { path: 'services/extension-equipo', component: ExtensionEquipoComponent },
       { path: 'about', component: AboutComponent },
-      { path: 'contact', component: ContactComponent }
+      { path: 'contact', component: ContactComponent },
     ],
   },
   { path: '**', redirectTo: '' },
-    
 ];
